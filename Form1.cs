abf8481b0b7daef65a70ff052fb4c672b478cc19
박경일 <one040422@gmail.com>
2026-03-12 17:16:59 +0900

    3단계(점수 계산과 성공시 버튼 크기 조절)

--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,11 @@
+using static System.Net.Mime.MediaTypeNames;
+
 namespace CatchButton
 {
     public partial class Form1 : Form
     {
+        int score = 0;
+        Random rd = new Random();
         public Form1()
         {
             InitializeComponent();
@@ -19,8 +23,6 @@ namespace CatchButton
 
         private void mybutton_MouseEnter(object sender, EventArgs e)
         {
-            Random rd = new Random();
-
             int maxX = rd.Next(0, this.ClientSize.Width - mybutton.Width);
             int maxY = rd.Next(0, this.ClientSize.Height - mybutton.Height);
 
@@ -29,12 +31,26 @@ namespace CatchButton
 
             mybutton.Location = new Point(nextX, nextY);
 
-            this.Text = $"버튼 위치: ({nextX}, {nextY})";
+            score -= 10;
+
+            this.Text = $"점수: {score} | 버튼 위치: ({nextX}, {nextY})";
         }
 
         private void mybutton_MouseClick(object sender, MouseEventArgs e)
         {
+            int btnX = mybutton.Location.X;
+            int btnY = mybutton.Location.Y;
+
             MessageBox.Show("축하합니다~!");
+
+            score += 100;
+
+            this.Text = $"점수: {score} | 버튼 위치: ({btnX}, {btnY})";
+
+            mybutton.Width = (int)(mybutton.Width * 0.95);
+            mybutton.Height = (int)(mybutton.Height * 0.95);
+            ;
+
         }
     }
 }
